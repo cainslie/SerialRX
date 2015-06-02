f08871a65bc5eaead87d36a52b44f6bcb3aa0849
cainslie <github@ainslie.co.za>
2015-06-02 14:44:01 +0200

    initial commit

---
+++ ConsoleApplication1/SerialRX.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.Ports;
+using System.Net;
+using System.Threading;
+using SharpDX.DirectInput;
+
+namespace SerialRX
+{
+    class SerialControl
+    {
+        static SerialPort serialPort = new SerialPort();
+        static int __xVal = 0;
+        static int __zVal = 0;
+        static bool __hasChanged = false;
+
+        static private int GetAxisValuePercentage(double maxValue, double currentValue)
+        {
+            return (int)Math.Round(currentValue * 100 / maxValue);
+        }
+
+        static private void ProcessJoystickUpdate(JoystickUpdate state)
+        {
+            if (state.Offset == JoystickOffset.X)
+            {
+                int xAxisPercent = GetAxisValuePercentage(65535, state.Value);
+                SetXVal(xAxisPercent);
+                __hasChanged = true;
+            }
+            if (state.Offset == JoystickOffset.Y)
+            {
+                int yAxisPercent = GetAxisValuePercentage(65535, state.Value);
+                __hasChanged = true;
+            }
+            else if (state.Offset == JoystickOffset.RotationZ)
+            {
+                int zAxisPercent = GetAxisValuePercentage(65535, state.Value);
+                SetZVal(zAxisPercent);
+                __hasChanged = true;
+            }
+            
+        }
+
+        static void SetupSerial()
+        {
+            serialPort.BaudRate = 9600;
+            serialPort.PortName = "COM9";
+            serialPort.Parity = Parity.None;
+            serialPort.StopBits = StopBits.One;
+            serialPort.Handshake = Handshake.None;
+
+            //**uncomment for live :)
+            //serialPort.Open();
+            //serialPort.Write("I");                                     //initialise device
+        }
+
+        static void SendCommand(string command)
+        {
+            //**uncomment for live :)
+            //serialPort.Write(command);
+            Console.WriteLine(command);
+        }
+
+        static void SetXVal(int xVal)
+        {
+            __xVal = xVal;
+        }
+
+        static void SetZVal(int zVal)
+        {
+            __zVal = zVal;
+        }
+
+        static int GetXVal()
+        {
+            return __xVal;
+        }
+
+        static int GetZVal()
+        {
+            return __zVal;
+        }
+
+        static string GetFinalValues()
+        {
+            int left = GetXVal();
+            int right = GetXVal();
+            if (GetZVal() > 50)
+            {
+                left = left + GetZVal();
+            }
+            if (GetZVal() < 50)
+            {
+                right = right - GetZVal();
+            }
+            return left.ToString() + "|" + right.ToString();
+        }
+
+        static void Main(string[] args)
+        {
+            SetupSerial();
+            CancellationTokenSource _joystickMonitorCancellation = new CancellationTokenSource();
+            var joystickMonitor = new JoystickMonitor("USB Gamepad");
+            var progress = new Progress<JoystickUpdate>(s => ProcessJoystickUpdate(s));
+            while (1==1) 
+            {
+                joystickMonitor.PollJoystick(progress, _joystickMonitorCancellation.Token);
+                if (__hasChanged)
+                {
+                    SendCommand(GetFinalValues());
+                }
+            }
+            
+        }
+    }
+}
+
---
+++ ConsoleApplication1/sJoy.cs
@@ -0,0 +1,47 @@
+﻿using SharpDX.DirectInput;
+using System;
+using System.Linq;
+using System.Threading;
+
+namespace SerialRX
+{
+    public class JoystickMonitor
+    {
+        private string _joystickName;
+
+        public JoystickMonitor(string joystickName)
+        {
+            _joystickName = joystickName;
+        }
+
+        public void PollJoystick(IProgress<JoystickUpdate> progress, CancellationToken cancellationToken)
+        {
+            var directInput = new DirectInput();
+
+            DeviceInstance device = directInput.GetDevices(DeviceType.Joystick, DeviceEnumerationFlags.AttachedOnly).Single();
+            device = directInput.GetDevices(DeviceType.Joystick, DeviceEnumerationFlags.AttachedOnly)
+                                          .SingleOrDefault(x => x.ProductName == device.ProductName);
+
+            if (device == null)
+            {
+                throw new Exception(string.Format("Can't find joystick:", _joystickName));
+            }
+
+            var joystick = new Joystick(directInput, device.InstanceGuid);
+
+            joystick.Properties.BufferSize = 128;
+            joystick.Acquire();
+
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                joystick.Poll();
+                JoystickUpdate[] states = joystick.GetBufferedData();
+
+                foreach (var state in states)
+                {
+                    progress.Report(state);
+                }
+            }
+        }
+    }
+}
